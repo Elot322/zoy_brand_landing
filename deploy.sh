6f9392477d99a90846e08ffa49729f34119f9df9
Maxim Podrezov <maximpodrezov@icloud.com>
2023-04-10 22:54:05 +0300

    Depl

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git master:gh-pages
+git push -f git@github.com:Elot322/zoy_brand_landing.git master:gh-pages
 cd -
